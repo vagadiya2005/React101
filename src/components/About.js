d28b1b2dea8347aa51d669b3f9ba1c211acbf9dc
Prince Vagadiya <147682826+vagadiya2005@users.noreply.github.com>
2024-06-12 22:08:44 +0530

    Update About.js
    Signed-off-by: Prince Vagadiya <147682826+vagadiya2005@users.noreply.github.com>

--- src/components/About.js
+++ src/components/About.js
@@ -55,7 +55,7 @@ backgroundColor: 'white',
     </h2>
     <div id="collapseOne" style={mystyle} className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div className="accordion-body" style={mystyle}>
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <strong>This is the first em's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
   </div>
